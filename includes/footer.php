752f382c2a086e24f83ef6f0be8cc74f5985f992
Ricardo R. Halog Jr. <79125209+RiczzIoT@users.noreply.github.com>
2025-08-03 21:12:12 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center py-8 mt-8 text-gray-400 text-sm">
+    <p>
+        Copyright &copy; <?php echo date('Y'); ?> 
+        <a href="https://github.com/RiczzIoT" target="_blank" class="font-medium text-purple-400 hover:underline">RiczzIoT</a>. 
+        All Rights Reserved.
+    </p>
+</footer>
